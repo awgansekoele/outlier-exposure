3ea54ff8324cddc2ae0d2ee8ce12dbe734e75012
adriaan.gansekoele <awgansekoele@gmail.com>
2020-04-16 22:06:06 +0200

    Test norm change.

--- CIFAR_Distance/test.py
+++ CIFAR_Distance/test.py
@@ -67,7 +67,7 @@ if 'allconv' in args.method_name:
 else:
     net = WideResNet(args.layers, num_classes, args.widen_factor, dropRate=args.droprate)
 
-net = DistanceNet(backbone=net, z_dim=args.z_dim, n_classes=num_classes)
+#net = DistanceNet(backbone=net, z_dim=args.z_dim, n_classes=num_classes)
 
 start_epoch = 0
 
@@ -128,8 +128,9 @@ def get_ood_scores(loader, in_dist=False):
             latent = net.get_latent(data)
             output = net.get_distances(latent)
 
-            #_score.append(to_np(-output.max(dim=1).values))
-            _score.append(to_np(-latent.norm(dim=1).pow(2)))
+            # _score.append(to_np(-output.max(dim=1).values))
+            # _score.append(to_np(-latent.norm(dim=1).pow(2)))
+            _score.append(to_np(-output.softmax(dim=1).max(dim=1).values))
 
             if in_dist:
                 preds = np.argmax(to_np(output), axis=1)
@@ -137,10 +138,13 @@ def get_ood_scores(loader, in_dist=False):
                 right_indices = preds == targets
                 wrong_indices = np.invert(right_indices)
 
-                #_right_score.append(to_np(-output.max(dim=1).values)[right_indices])
-                #_wrong_score.append(to_np(-output.max(dim=1).values)[wrong_indices])
-                _right_score.append(to_np(-latent.norm(dim=1).pow(2))[right_indices])
-                _wrong_score.append(to_np(-latent.norm(dim=1).pow(2))[wrong_indices])
+                _right_score.append(to_np(-output.softmax(dim=1).max(dim=1).values)[right_indices])
+                _wrong_score.append(to_np(-output.softmax(dim=1).max(dim=1).values)[wrong_indices])
+
+                # _right_score.append(to_np(-output.max(dim=1).values)[right_indices])
+                # _wrong_score.append(to_np(-output.max(dim=1).values)[wrong_indices])
+                # _right_score.append(to_np(-latent.norm(dim=1).pow(2))[right_indices])
+                # _wrong_score.append(to_np(-latent.norm(dim=1).pow(2))[wrong_indices])
 
     if in_dist:
         return concat(_score).copy(), concat(_right_score).copy(), concat(_wrong_score).copy()
