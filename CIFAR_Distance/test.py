2a6c80b4f13842322947665e54b60e8bcdbe9237
adriaan.gansekoele <awgansekoele@gmail.com>
2020-04-16 19:04:05 +0200

    Implemented test functionality.

--- CIFAR_Distance/test.py
+++ CIFAR_Distance/test.py
@@ -125,10 +125,9 @@ def get_ood_scores(loader, in_dist=False):
 
             data = data.to(device)
 
-            latent = net.get_latent(data)
-            output = net.get_distances(latent)
+            output = net(data)
 
-            _score.append(to_np(-latent.norm(dim=1).pow(2)))
+            _score.append(to_np(-output.max(dim=1).values))
 
             if in_dist:
                 preds = np.argmax(to_np(output), axis=1)
@@ -136,8 +135,10 @@ def get_ood_scores(loader, in_dist=False):
                 right_indices = preds == targets
                 wrong_indices = np.invert(right_indices)
 
-                _right_score.append(to_np(-latent.norm(dim=1).pow(2))[right_indices])
-                _wrong_score.append(to_np(-latent.norm(dim=1).pow(2))[wrong_indices])
+                _right_score.append(to_np(-output.max(dim=1).values)[right_indices])
+                _wrong_score.append(to_np(-output.max(dim=1).values)[wrong_indices])
+                #_right_score.append(to_np(-latent.norm(dim=1).pow(2))[right_indices])
+                #_wrong_score.append(to_np(-latent.norm(dim=1).pow(2))[wrong_indices])
 
     if in_dist:
         return concat(_score).copy(), concat(_right_score).copy(), concat(_wrong_score).copy()
