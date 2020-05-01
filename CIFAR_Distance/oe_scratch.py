94c023a36975efb08c40696292a9d752b5a0a69a
adriaan.gansekoele <awgansekoele@gmail.com>
2020-05-01 17:31:39 +0200

    Test cosine

--- CIFAR_Distance/oe_scratch.py
+++ CIFAR_Distance/oe_scratch.py
@@ -176,9 +176,9 @@ def train():
         scheduler.step()
         optimizer.zero_grad()
 
-        loss = torch.gather((1-o[:len(in_set[0])]).pow(2), 1, target.view(-1, 1)).mean()
+        loss = torch.gather((1-o[:len(in_set[0])]).pow(2), 1, target.view(-1, 1)).sum()
         # distance of latent vector to origin
-        loss += (1 - o[len(in_set[0]):, int(o.size(1)/2):].max(dim=1).values).pow(2).mean()
+        loss += (1 - o[len(in_set[0]):, int(o.size(1)/2):].max(dim=1).values).pow(2).sum() / 2
 
         loss.backward()
         optimizer.step()
