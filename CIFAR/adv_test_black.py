6af4702c163359c5481a5af08c1bbd008df458a1
adriaan.gansekoele <awgansekoele@gmail.com>
2020-06-08 12:23:36 +0200

    Blackbox test.

--- CIFAR/adv_test_black.py
+++ CIFAR/adv_test_black.py
@@ -13,7 +13,6 @@ import torchvision.datasets as dset
 import torch.nn.functional as F
 from models.allconv import AllConvNet
 from models.wrn import WideResNet
-from models.distnet import DistanceNet
 from skimage.filters import gaussian as gblur
 from PIL import Image as PILImage
 
--- CIFAR/experiments/10adv_test_black.sh
+++ CIFAR/experiments/10adv_test_black.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-python adv_test.py --method_name cifar10_wrn_oe_scratch --num_to_avg 10 > snapshots/cifar10_wrn_oe_scratch_adv_test.txt
+python adv_test_black.py --dataset cifar10 --num_to_avg 10 > snapshots/cifar10_wrn_oe_scratch_adv_test_black.txt
 # allconv_oe_scratch, wrn_baseline, wrn_oe_tune
 
