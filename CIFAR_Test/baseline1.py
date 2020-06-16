14d019a686e4fb11bcabae8b8fef3ee286a45b13
adriaan.gansekoele <awgansekoele@gmail.com>
2020-06-16 19:55:03 +0200

    Test for failed experiments.

--- CIFAR_Test/baseline1.py
+++ CIFAR_Test/baseline1.py
@@ -83,6 +83,7 @@ test_loader = torch.utils.data.DataLoader(
 
 net = resnet18(pretrained=True)
 net.fc = nn.Linear(net.fc.in_features, args.z_dim)
+net.fc.weight = net.fc.weight / args.z_dim
 net = NaiveNet(backbone=net, z_dim=args.z_dim, n_classes=num_classes)
 experiment.set_model_graph(str(net), overwrite=True)
 
