a0c7befdd193ba5d65a3788168d1fec78f36aead
adriaan.gansekoele <awgansekoele@gmail.com>
2020-06-26 19:40:28 +0200

    Test for CE

--- CIFAR_Distance2/test.py
+++ CIFAR_Distance2/test.py
@@ -138,15 +138,15 @@ def get_ood_scores(loader, in_dist=False):
 
             output = net(data)
 
-            _score.append(-to_np(output.data.softmax(dim=1).max(1).values))
+            _score.append(-to_np(output.data.max(1).values))
             if in_dist:
                 preds = np.argmax(to_np(output), axis=1)
                 targets = target.numpy().squeeze()
                 right_indices = preds == targets
                 wrong_indices = np.invert(right_indices)
 
-                _right_score.append(-to_np(output.data.softmax(dim=1).max(1).values)[right_indices])
-                _wrong_score.append(-to_np(output.data.softmax(dim=1).max(1).values)[wrong_indices])
+                _right_score.append(-to_np(output.data.max(1).values)[right_indices])
+                _wrong_score.append(-to_np(output.data.max(1).values)[wrong_indices])
 
     if in_dist:
         return concat(_score).copy(), concat(_right_score).copy(), concat(_wrong_score).copy()
